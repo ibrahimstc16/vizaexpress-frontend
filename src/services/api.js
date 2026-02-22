02550396929d0593dd865e74f9d65a0f2ff12bde
Ibrahim <isutcu5@gmail.com>
2026-02-22 08:48:47 +0100

    Switch to Railway backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://vizaexpress-backend.onrender.com/api';
+const API_URL = 'https://vizaexpress-backend-production.up.railway.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
